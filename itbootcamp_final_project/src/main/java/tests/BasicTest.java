1b7b789d075f42b7693f8a97c23a25202cd35127
Sandra Milic <sandra.psihosfera@gmail.com>
2022-06-23 11:19:48 +0200

    'Basic Test' Update - 'AfterMethod' for failed tests

--- itbootcamp_final_project/src/main/java/tests/BasicTest.java
+++ itbootcamp_final_project/src/main/java/tests/BasicTest.java
@@ -1,14 +1,22 @@
 package tests;
 
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.time.Duration;
+import java.util.Date;
 
 
 public abstract class BasicTest {
@@ -38,11 +46,24 @@ public abstract class BasicTest {
     @BeforeMethod
     public void beforeMethod() {
         this.driver.get(baseUrl);
+        this.driver.manage().window().maximize();
     }
 
     @AfterMethod
-    public void afterMethod(){
+    public void tearDown(ITestResult result) {
+        String timestamp = new SimpleDateFormat(
+                "hh_mm_ss_dd_MM_yyyy").format(new Date());
 
+        if (result.getStatus() == ITestResult.FAILURE) {
+            TakesScreenshot screen = (TakesScreenshot) driver;
+            File screenshot = screen.getScreenshotAs(OutputType.FILE);
+            try {
+                FileHandler.copy(screenshot, new File(
+                        "src/main/java/Screenshots/TestFailure_" + timestamp + ".png"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @AfterClass
